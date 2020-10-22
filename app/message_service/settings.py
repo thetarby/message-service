e678823776fa6ef9875a73e9db9ea419ebd6834b
Furkan Akyol <furkanakyol1997@gmail.com>
2020-10-22 20:40:51 +0300

    production ready configuration

--- app/message_service/settings.py
+++ app/message_service/settings.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-# TODO: build ederken migrate etmeyi unutma
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -130,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 REST_FRAMEWORK = {
