7b061ba4aa36e75b0878cfa336b8990cfba1576e
Furkan Akyol <furkanakyol1997@gmail.com>
2020-10-22 21:06:37 +0300

    bug fix: localhost added to allowed hosts

--- app/message_service/settings.py
+++ app/message_service/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 't1*gu2%(2g=p!a6!(z3%3ctc86s1))v3aktch!y$#!+($kfr+6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
 
 # Application definition
